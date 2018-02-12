49916a78de4912f01492e9a8d89c0221f1785ba0
Kira Helm <soulclimberchick@gmail.com>
2018-02-11 17:18:07 -0800

    ckeckpoint 5 Library: completed

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: albumData };
+  }
+
+  render () {
+    return (
+      <section className='Library'>
+        {
+          this.state.albums.map( (album, index) =>
+            <Link to={`/album/${album.slug}`} key={index}>
+              <img src={album.albumCover} alt={album.title} />
+              <div>{album.title}</div>
+              <div>{album.artist}</div>
+              <div>{album.songs.length}</div>
+            </Link>
+          )
+        }
+      </section>
+    );
+  }
+}
 
 export default Library;
