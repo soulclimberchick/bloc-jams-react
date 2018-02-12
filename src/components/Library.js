036a3ed0e5ff81bfc70b8e71d86ea5f4f1d2a78e
Kira Helm <soulclimberchick@gmail.com>
2018-02-11 18:41:19 -0800

    Assignment 6 Album: completed

--- src/components/Library.js
+++ src/components/Library.js
@@ -16,12 +16,12 @@ class Library extends Component {
       <section className="library">
         {
           this.state.albums.map( (album, index) =>
-            <Link to={`/album/${album.slug}`} key={index}>
+            <div key={index} >
               <img src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
-            </Link>
+            </div>
           )
         }
       </section>
--- src/components/album.js
+++ src/components/album.js
@@ -6,37 +6,52 @@ class Album extends Component {
     super(props);
 
     const album = albumData.find( album => {
-      return album.slug === this.props.match.params.slug
-    });
+       return album.slug === this.props.match.params.slug;
+     });
 
-    this.state = {
-      album: album
-    };
-  }
+     this.state = {
+       album: album
+     };
 
-  render () {
-    return (
-      <section className="album">
-      <section id="album-info">
-         <img id="album-cover-art" src={this.state.album.albumCover} />
-         <div className="album-details">
-         <h1 id="album-title">{this.state.album.title}</h1>
-         <h2 className="artist">{this.state.album.artist}</h2>
-         <div id="release-info">{this.state.album.releaseInfo}</div>
-         </div>
-       </section>
-        <table id="song-list">
+     this.audioElement = document.createElement('audio');
+   }
+
+  render() {
+     return (
+       <section className="album">
+         <section id="album-info">
+           <img id="album-cover-art" src={this.state.album.albumCover} />
+           <div className="album-details">
+             <h1 id="album-title">{this.state.album.title}</h1>
+             <h2 className="artist">{this.state.album.artist}</h2>
+             <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
+           </div>
+         </section>
+         <table id="song-list">
            <colgroup>
              <col id="song-number-column" />
              <col id="song-title-column" />
              <col id="song-duration-column" />
            </colgroup>
            <tbody>
+             {this.state.album.songs.map( (song, index) =>
+               <tr className="song" key={index} >
+                 <td className="song-actions">
+                   <button>
+                     <span className="song-number">{index+1}</span>
+                     <span className="ion-play"></span>
+                     <span className="ion-pause"></span>
+                   </button>
+                 </td>
+                 <td className="song-title">{song.title}</td>
+                 <td className="song-duration">{song.duration}</td>
+               </tr>
+             )}
            </tbody>
          </table>
-      </section>
-    );
-  }
-}
+       </section>
+     );
+   }
+ }
 
 export default Album;
