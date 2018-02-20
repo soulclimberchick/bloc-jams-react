2d7929a0dc9b102e2618179a0a20c359d05d99e3
Kira Helm <soulclimberchick@gmail.com>
2018-02-19 17:49:42 -0800

    Moved td tag down a line

--- src/components/album.js
+++ src/components/album.js
@@ -63,7 +63,8 @@ class Album extends Component {
            </colgroup>
            <tbody>
              {this.state.album.songs.map( (song, index) =>
-               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >                 <td className="song-actions">
+               <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                <td className="song-actions">
                    <button>
                      <span className="song-number">{index+1}</span>
                      <span className="ion-play"></span>
