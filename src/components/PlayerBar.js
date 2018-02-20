5bd8c5e3e9dae7b2e80d9e0344db83e62d21997b
Kira Helm <soulclimberchick@gmail.com>
2018-02-19 19:14:11 -0800

    Checkpoint 9 Player Bar: Completed   Forgot to checkout a branch, also my filesystem doesn't want to keep a capitol A for the album.js file so I adjusted things again accordingly. This may break things on previous commits

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -16,9 +16,17 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">-:-</div>
-          <input type="range" className="seek-bar" value="80" />
-          <div className="ivon ion-volume-high"></div>
+          <div className="current-time">{this.props.currentTime}</div>
+          <input
+          type="range"
+          className="seek-bar"
+          value={(this.props.currentTime / this.props.duration) || 0}
+          max="1"
+          min="0"
+          step="0.01"
+          onChange={this.props.handleTimeChange}
+           />
+          <div className="total-time">{this.props.duration}</div>
         </section>
       </section>
     );
